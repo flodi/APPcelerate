29d69b7d32678e3c08cbe2128bc119ed38a27253
Fabrizio Lodi <flodi@thinpower.local>
2017-01-25 17:23:15 +0100

    init

--- src/appcelerate.php
+++ src/appcelerate.php
@@ -1,6 +1,6 @@
 <?
 /*
- * (c) YOUR NAME <flodi@e-scientia.eu>
+ * (c) Fabrizio Lodi <flodi@e-scientia.eu>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
