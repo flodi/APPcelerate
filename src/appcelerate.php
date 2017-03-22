d08d984e7f703534db170be48a15077e761f01d4
Fabrizio Lodi <flodi@thinpower.local>
2017-03-22 14:04:45 +0100

    Specific security ko

--- src/appcelerate.php
+++ src/appcelerate.php
@@ -391,15 +391,15 @@ class APPcelerate {
 							$this->app["locale"]=$rs1->fetch_row()[0];
 							$_SESSION[$this->app["name"]."_ap_locale"]=$this->app['locale'];
 						}
-						$this->doLog("[SECURITY] redirecting to ".$this->app["base_url"]."/".$this->app["name"]."/");
+						$this->doLog("[SECURITY OK] redirecting to ".$this->app["base_url"]."/".$this->app["name"]."/");
 						header("Location: ".$this->app["base_url"]."/".$this->app["name"]."/");
 						die();
 					case 0:
-						$this->doLog("[SECURITY] redirecting to ".$this->app["base_url"]."/".$this->app["name"]."/login/?wrong");
+						$this->doLog("[SECURITY KO] redirecting to ".$this->app["base_url"]."/".$this->app["name"]."/login/?wrong");
 						header("Location: ".$this->app["base_url"]."/".$this->app["name"]."/login/?wrong");
 						die();
 					default:
-						$this->doLog("[SECURITY] redirecting to ".$this->app["base_url"]."/".$this->app["name"]."/login/?wrong");
+						$this->doLog("[SECURITY MULTI] redirecting to ".$this->app["base_url"]."/".$this->app["name"]."/login/?wrong");
 						header("Location: ".$this->app["base_url"]."/".$this->app["name"]."/login/?multi");
 						die();
 				}
