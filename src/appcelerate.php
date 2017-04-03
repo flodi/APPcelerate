e473f0ae498927c759a29bc9aa22d363ee2aa2c2
Fabrizio Lodi <flodi@thinpower.local>
2017-04-03 11:06:27 +0200

    fix

--- src/appcelerate.php
+++ src/appcelerate.php
@@ -186,10 +186,10 @@ class APPcelerate {
 
 		$sql="CREATE TABLE $tmptable (id INT NOT NULL AUTO_INCREMENT,";
 		foreach ($columns as $key => $value) {
-			$sql.="$value text,";
+			$sql.="`$value` text,";
 		}
 		foreach ($addcolumns as $key => $value) {
-			$sql.="$value text,";
+			$sql.="`$value` text,";
 		}
 		$sql.="PRIMARY KEY (id));";
 		$rs=$this->app["db_".$this->app["name"]]->query($sql);
