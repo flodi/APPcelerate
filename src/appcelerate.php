ac210cad5ac246883e608c0e93d8cbef412fb20b
flodi <flodi@e-scientia.eu>
2017-06-13 14:55:35 +0200

    fix

--- src/appcelerate.php
+++ src/appcelerate.php
@@ -1488,7 +1488,6 @@ class BPME {
 		}
 		$sql=sprintf("insert into activity_instances (id_activity,id_process,id_process_instance,id_actor_created,id_actor_assigned) values (%d,%d,%d,%d,%s)",$id_activity,$id_process,$id_process_instance,$uid,$id_actor_assigned);
 
-echo $sql;
 		$rs=$this->db->query($sql);
 		try {
 			$this->rsCheck($rs);
