224c94f518a1e424bd4bf5a7f53884aee3223c2b
flodi <flodi@e-scientia.eu>
2017-06-19 12:45:49 +0200

    fix

--- src/appcelerate.php
+++ src/appcelerate.php
@@ -1093,7 +1093,7 @@ class BPME {
 
 	private function getLastActivities() {
 		$uid=$getCurrentUID();
-		$sql="select * from activity_instances where date_completed is null and (id_actor_assigned is null or id_actor_assigned=$uid) order by date_created desc limit 10";
+		$sql="select * from activity_instances where date_completed is null and (id_actor_assigned is null or id_actor_assigned=$uid or id_actor_created=$uid) order by date_created desc limit 10";
 		$rs=$this->db->query($sql);
 		try {
 			$this->rsCheck($rs);
