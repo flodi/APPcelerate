ad222ae53aa2e732f73332fa00f8a6d4540927e2
flodi <flodi@e-scientia.eu>
2017-06-28 11:23:07 +0200

    fix

--- src/bpme.php
+++ src/bpme.php
@@ -1,4 +1,11 @@
 <?
+/*
+ * (c) Fabrizio Lodi <flodi@e-scientia.eu>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 class BPME {
 
 	private $app_name;
