ee206fdcef649387a9367a1a112ac5e510c9d34c
flodi <flodi@e-scientia.eu>
2018-03-01 12:32:10 +0100

    fix
    Signed-off-by: flodi <flodi@e-scientia.eu>

--- src/appcelerate.php
+++ src/appcelerate.php
@@ -605,7 +605,6 @@ class APPcelerate {
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 						';
 					}
-					';
 					break;
 			}
 
