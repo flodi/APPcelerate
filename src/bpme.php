a040bee9c086577a5891a61f599c1a196cb36eca
flodi <flodi@e-scientia.eu>
2018-05-11 12:06:01 +0200

    fix

--- src/bpme.php
+++ src/bpme.php
@@ -1235,7 +1235,7 @@ class BPME {
 		$id_process_instance=$this->getProcessInstanceFromActivityInstance($id_activity_instance_from);
 
 		//Concludo l'activity precedente ed eseguo closing script, se non era già conclusa
-		$sql="select id from activity_instances where date_completed is not null and id_actor_completed is not null and id=$id_activity_instance_from";
+		$sql="select id from activity_instances where date_completed is null and id_actor_completed is null and id=$id_activity_instance_from";
 		$rs=$this->db->query($sql);
 		try {
 			$this->rsCheck($rs);
