031bdc796b4c90a581c98130bce60b65ac8e6386
flodi <io@fabriziolodi.com>
2019-12-09 12:42:20 +0100

    renew

--- src/appcelerate.php
+++ src/appcelerate.php
@@ -667,7 +667,7 @@ class APPcelerate {
 					}
 					if ($this->app["bootstrap"]==4) {
 						$c.='
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.js"></script>
 						';
 					}
 					$c.='
