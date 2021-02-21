dbbbf7fc27f1f469823fd74030b55e00bf4a1d21
flodi <io@fabriziolodi.com>
2021-02-21 23:28:46 +0100

    default libs update

--- src/appcelerate.php
+++ src/appcelerate.php
@@ -697,7 +697,7 @@ class APPcelerate {
 					}
 					if ($this->app["bootstrap"]==5) {
 						$c.='
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.bundle.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 		<link href="https://unpkg.com/tabulator-tables@4.9.3/dist/css/tabulator.min.css" rel="stylesheet">
 						';
 					}
