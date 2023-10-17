733c99ccdbc0d577e4df68bd6024371e24d9fb1f
flodi <flodi@e-scientia.eu>
2023-10-17 12:16:50 +0200

    test

--- src/appcelerate.php
+++ src/appcelerate.php
@@ -986,7 +986,7 @@ class APPcelerate {
 					}
 					$rs=$this->app["db_".$this->app["name"]]->query($sql);
 					$this->sqlError($rs,$sql);
-					$r=$rs->fetch_row();
+					$r=$rs->fetch_row(MYSQLI_NUM);
 					if ($rs->num_rows==1) {
 						$this->app['uid']=$r[0];
 						$hash=$r[1];
